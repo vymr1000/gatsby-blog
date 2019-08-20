33ace19a9417d37ca6e851eefba6f41dd8843075
Lee Sun Hong <nuseehr@naver.com>
2019-08-20 23:55:34 +0900

    Update main font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Alexander-boykin Dev Story`,
+    title: `Dev Ceremony`,
     author: `Alexander Boykin`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,24 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+// Wordpress2016.overrideThemeStyles = () => {
+//   return {
+//     "a.gatsby-resp-image-link": {
+//       boxShadow: `none`,
+//     },
+//   }
+// }
 
-delete Wordpress2016.googleFonts
+// delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+// const typography = new Typography(Wordpress2016)
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Nanum Myeongjo"],
+  bodyFontFamily: ["Nanum Gothic Coding"],
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
