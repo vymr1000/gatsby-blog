96e5d283a9ec56b1c9c52b7624ed5b495ec813b4
Lee Sun Hong <nuseehr@naver.com>
2019-08-26 23:20:32 +0900

    Update bio

--- config.js
+++ config.js
@@ -1,46 +1,47 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Alexander Boykin",
+  subtitle:
+    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
+      label: "Contact me",
+      path: "/pages/contacts",
+    },
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Alexander Boykin",
+    photo: "/flight-plan.jpg",
+    bio: "스스로 만족하는 Front-end Engineer가 되기 위하여.",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: ''
-    }
-  }
-};
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+    },
+  },
+}
--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -1,40 +1,34 @@
 // @flow strict
-import React from 'react';
-import { withPrefix, Link } from 'gatsby';
-import styles from './Author.module.scss';
+import React from "react"
+import { withPrefix, Link } from "gatsby"
+import styles from "./Author.module.scss"
 
-type Props = {
-  author: {
-    name: string,
-    bio: string,
-    photo: string
-  },
-  isIndex: ?boolean
-};
-
-const Author = ({ author, isIndex }: Props) => (
-  <div className={styles['author']}>
+const Author = ({ author, isIndex }) => (
+  <div className={styles["author"]}>
     <Link to="/">
       <img
         src={withPrefix(author.photo)}
-        className={styles['author__photo']}
+        className={styles["author__photo"]}
         width="75"
-        height="75"
         alt={author.name}
       />
     </Link>
 
-    { isIndex === true ? (
-      <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+    {isIndex === true ? (
+      <h1 className={styles["author__title"]}>
+        <Link className={styles["author__title-link"]} to="/">
+          {author.name}
+        </Link>
       </h1>
     ) : (
-      <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+      <h2 className={styles["author__title"]}>
+        <Link className={styles["author__title-link"]} to="/">
+          {author.name}
+        </Link>
       </h2>
     )}
-    <p className={styles['author__subtitle']}>{author.bio}</p>
+    <p className={styles["author__subtitle"]}>{author.bio}</p>
   </div>
-);
+)
 
-export default Author;
+export default Author
